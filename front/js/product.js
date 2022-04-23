0e2e959283cb9213f0c998fec890e71c8bbfafb9
Helene Pagniez <helenepagniez51@gmail.com>
2022-04-23 16:58:40 +0200

    ajouter produits dans panier + rassemblement produits si identiques + enlever liens inutiles

--- front/js/product.js
+++ front/js/product.js
@@ -2,7 +2,8 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 
-let url = 'http://localhost:3000/api/products/' + id; /*url API*/
+//url API
+let url = 'http://localhost:3000/api/products/' + id; 
 
 var myHeaders = new Headers();
 
@@ -42,18 +43,95 @@ function showProduct(product)
     let name = document.getElementById("title");
     let prix = document.getElementById("price");
     let description = document.getElementById("description");
-    let couleurOption = document.getElementById("colors");
+    let colorOption = document.getElementById("colors");
 
     //apparition dynamique des éléments
     imageAlt.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
     name.innerHTML += product.name;
     prix.textContent = `${product.price} `;
     description.innerHTML += product.description;
-    couleurOption.innerHTML += product.couleurOption;
+    colorOption.innerHTML += product.colorOption;
 
-    // choix couleurs dynamiques
-    for (let couleur of product.colors) 
+    //choix colors dynamiques
+    for (let color of product.colors) 
     {
-        couleurOption.innerHTML += `<option value="${couleur}">${couleur}</option>`;
+        colorOption.innerHTML += `<option value="${color}">${color}</option>`;
     }
-}
+
+    let button = document.getElementById("addToCart");
+    button.addEventListener('click', function()
+    {
+        let stockProduct=document.getElementById("quantity");
+        let colorOption=document.getElementById("colors");
+
+        if ((stockProduct.value === "0") 
+            || (colorOption.value === ""))
+        {
+            alert("Veuillez sélectionner une couleur et une quantité");
+        }
+        else
+        {
+            //Recupération choix de la color
+            let colorPicked = colorOption.value;
+                
+            //Recupération choix de la quantité
+            let quantity = stockProduct.value;
+
+            //Récupération des options de l'article à ajouter au panier
+            let productOptions = 
+            {
+                id: id,
+                colorPicked: colorPicked,
+                quantity: quantity,
+                productName: product.name,
+                productPrice: product.price,
+                productDescription: product.description,
+                productImg: product.imageUrl,
+                productAltImg: product.altTxt
+            };
+
+            let productList = JSON.parse(localStorage.getItem("productList"));
+            
+            if (productList) 
+            {
+
+                const resultFind = productList.find(
+                    (product) => product.id === id && product.colorPicked === colorPicked);
+                    
+                    //Si le produit commandé est déjà dans le panier
+                    if (resultFind) 
+                    {
+                        resultFind.quantity = parseInt(resultFind.quantity) + parseInt(productOptions.quantity);
+                        localStorage.setItem("productList", JSON.stringify(productList));
+                    }
+                    
+                    //Si le produit commandé n'est pas dans le panier
+                    else 
+                    {
+                        productList.push(productOptions);
+                        localStorage.setItem("productList", JSON.stringify(productList));
+                    }
+            }
+            else 
+            {
+                productList=[];
+                productList.push(productOptions);
+                localStorage.setItem("productList", JSON.stringify(productList));
+                console.table(productList);
+            }
+            
+            //alerte pop-up
+            var result = confirm("Le produit a bien été ajouté à votre panier\n Voulez-vous aller directement au panier ?", "confirmez");
+            
+            // résultat et chargement page
+            if (result === true)
+            {
+                window.location.href = "./cart.html"
+            }
+            else
+            {
+                window.location.href ="./index.html"
+            }
+        }
+    });
+};
